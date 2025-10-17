6d7897cd73dff205c66ec7b486d9e99f486b3cc4
Karli Munoz <146567084+kamuz-01@users.noreply.github.com>
2025-10-17 10:14:57 -0300

    commit classe Produto

--- src/main/java/org/main/models/Produto.java
+++ src/main/java/org/main/models/Produto.java
@@ -1,9 +1,7 @@
 package org.main.models;
 
 import java.time.LocalDateTime;
-
 import org.main.enums.StatusProduto;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
